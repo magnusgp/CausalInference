878047cd61f559b43aa9a64153d6b677251d8f26
dinbaba <s204075@student.dtu.dk>
2022-04-21 14:12:32 +0200

    main script importing model

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from sampleLoad import sampleLoad
+from structEqModel import structEqModel
+
 # Main script to run helper functions that will infer the graph
 
 if __name__ == '__main__':
-    print('Hello World!')
-    pass
+    model = structEqModel(n=1000)
+    model.loadData(path='sample/data_435.csv')
+    #print(model.data)
