0db97c3dc50a8dfa00a33f905ba2dde755553821
benj3542 <s200431@student.dtu.dk>
2022-04-24 20:51:17 +0200

    Creation of current guess function

--- Load.PD.py
+++ Load.PD.py
@@ -1,6 +1,8 @@
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 def CSV_load(filename):
     df = pd.read_csv(filename)
@@ -16,10 +18,15 @@ def plot_col_hist(df, col):
     df.hist()
     plt.show()
 
+def current_guess(image):
+    img = mpimg.imread(image)
+    imgplot = plt.imshow(img)
+    plt.show()
 
 
 
 if __name__ == '__main__':
-    data = "./sample/data_445.csv"
+    data = "./sample/data_435.csv"
     data_CSV = CSV_load(data)
     plot_col_hist(data_CSV, 4)
+    current_guess("./plots/artwork.jpg")
